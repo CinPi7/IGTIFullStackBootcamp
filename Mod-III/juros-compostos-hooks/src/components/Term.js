e269757a8f6fcea9e884687442ebd6edc108a3a5
Cinthia <49377882+CINPIS@users.noreply.github.com>
2020-08-22 13:54:28 -0300

    Add files via upload

---
+++ Mod-III/juros-compostos-hooks/src/components/Term.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const currencyFormatter = Intl.NumberFormat('pt-BR', {
+    style: 'currency', currency: 'BRL'
+});
+
+const percentageFormatter = Intl.NumberFormat('pt-BR', {
+    style: 'percent', maximunFractionDigits: 2
+});
+
+function formatCurrency(value){
+    return currencyFormatter.format(value);
+}
+
+function formatPercentage(value){
+    return percentageFormatter.format(value);
+}
+
+export default function Term({ data }) {
+
+    const { id, total, interest, percentage } = data;
+
+    let classValue = interest < 0 ? "red-text" : "light-green-text";
+    let classPercentage = interest < 0 ? "orange-text" : "ligth-blue-text";
+    let classBold = { fontWeight: 800 }
+
+    return (
+        <div className="col s2">
+            <div style={{ paddingLeft: "10px" }} className="z-depth-1">
+                <div className="row card s12 m8">
+                <div style={classBold} className="col">{id}</div>
+                    <div className="col">
+                        <div style={classBold} className={classValue}>
+                            {formatCurrency(total)}
+                        </div>
+                        <div style={classBold} className={classValue}>
+                            {formatCurrency(interest)}
+                        </div>
+                        <div className={classPercentage}>
+                            {formatPercentage(percentage)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
